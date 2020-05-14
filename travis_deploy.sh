915a797835bc9d10b9b8189cea489b590632dffb
like <jacksonlike290@163.com>
2020-05-14 22:25:51 +0800

    Test travis deploy

---
+++ travis_deploy.sh
@@ -0,0 +1,9 @@
+BUNDLE_DIR="/server/ci-versions/blog"
+BUNDLE="blog.tar.gz"
+NGINX_DIR="/server/blog"
+
+cd ${BUNDLE_DIR}
+tar -zxf ${BUNDLE} 
+rm -f ${BUNDLE}
+find . -maxdepth 1 -type d -mtime +365 -exec -rm -rf {} \;
+ln -s ${BUNDLE_DIR}/re_VERSION ${NGINX_DIR}
